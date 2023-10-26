7a28424596d94d36c7d7d14a66a8cffd97983690
Deepak Bhalla <deepak.dbhalla@gmail.com>
2023-10-26 14:19:59 +0530

    Add files via upload

---
+++ src/main/java/com/example/springbootjasyptencryptiondecryption/SpringbootJasyptEncryptionDecryptionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjasyptencryptiondecryption;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJasyptEncryptionDecryptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootJasyptEncryptionDecryptionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springbootjasyptencryptiondecryption/SpringbootJasyptEncryptionDecryptionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springbootjasyptencryptiondecryption;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootJasyptEncryptionDecryptionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
